afe1f1d05cd710ad2b3b0b8043ffe058f55de91b
Rasmussen <Peter.Rasmussen@state.mn.us>
2019-09-04 12:45:36 -0500

    Module 2: clip 5 - Template Variables

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core'
             <h1>Upcoming Angular Events</h1>
             <hr/>
             <event-thumbnail [event]="event1"></event-thumbnail>
+
         </div>
     `
 })
