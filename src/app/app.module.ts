bf33c996de6c218d6a18c147fa1c77ac397beacc
Rasmussen <Peter.Rasmussen@state.mn.us>
2019-09-05 10:35:11 -0500

    Module 4: clip 2 - Creating your first service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,19 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavbarComponent } from './nav/navbar.component'
+import { EventService } from './events/shared/event.service'
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  declarations: [
-    EventsAppComponent,
-    EventsListComponent,
-    EventThumbnailComponent,
-    NavbarComponent
-  ],
-  bootstrap: [EventsAppComponent]
+    imports: [
+      BrowserModule
+    ],
+    declarations: [
+      EventsAppComponent,
+      EventsListComponent,
+      EventThumbnailComponent,
+      NavbarComponent
+    ],
+    providers: [EventService],
+    bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
