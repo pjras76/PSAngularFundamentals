8a779b231fa5b21d3b2a2c430c556ff0fb06f611
Rasmussen <Peter.Rasmussen@state.mn.us>
2019-09-05 11:08:35 -0500

    Module 4: clip 3 - Wrapping 3rd party services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavbarComponent } from './nav/navbar.component'
 import { EventService } from './events/shared/event.service'
+import { ToastrService } from './common/toastr.service';
 
 @NgModule({
     imports: [
@@ -17,7 +18,10 @@ import { EventService } from './events/shared/event.service'
       EventThumbnailComponent,
       NavbarComponent
     ],
-    providers: [EventService],
+    providers: [
+      EventService,
+      ToastrService
+    ],
     bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
