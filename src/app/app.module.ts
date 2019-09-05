4e25aeb98a56e741388d266abfe1fc513a2a5f07
Rasmussen <Peter.Rasmussen@state.mn.us>
2019-09-05 15:42:26 -0500

    Module 5: clip 8 - Pre-loading data for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EventDetailsComponent } from './events/event-details/event-details.comp
 import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
 import { EventRouteActivator } from './events/event-details/event-route-activator.service'
+import { EventListResolver } from './events/events-list-resolver.service'
 import { appRoutes } from './routes';
 
 
@@ -33,6 +34,7 @@ import { appRoutes } from './routes';
 		EventService,
 		ToastrService,
 		EventRouteActivator,
+		EventListResolver,
 		{ 
 			provide: 'canDeactivateCreateEvent', 
 			useValue: checkDirtyState 
