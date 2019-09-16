260bc2ce0024c698636a62ca44ef6d301b760240
Rasmussen <Peter.Rasmussen@state.mn.us>
2019-09-16 12:32:45 -0500

    Module 9: clip 2 - Creating custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import {
 	EventRouteActivator,
 	EventListResolver,
 	CreateSessionComponent,
-	SessionListComponent
+	SessionListComponent,
+	DurationPipe
 } from './events/index'
 import { EventsAppComponent } from './events-app.component';
 import { NavbarComponent } from './nav/navbar.component';
@@ -39,7 +40,8 @@ import { appRoutes } from './routes';
 		Error404Component,
 		CreateSessionComponent,
 		SessionListComponent,
-		CollapsibleWellComponent
+		CollapsibleWellComponent,
+		DurationPipe
     ],
     providers: [
 		EventService,
