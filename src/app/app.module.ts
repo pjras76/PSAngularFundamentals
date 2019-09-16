237a3c2eb754ec52fda958eeb83e69e802fab825
Rasmussen <Peter.Rasmussen@state.mn.us>
2019-09-16 09:59:53 -0500

    Module 7: clip 1 - Passing data into child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import {
 	CreateEventComponent,
 	EventRouteActivator,
 	EventListResolver,
-	CreateSessionComponent
+	CreateSessionComponent,
+	SessionListComponent
 } from './events/index'
 import { EventsAppComponent } from './events-app.component';
 import { NavbarComponent } from './nav/navbar.component';
@@ -36,7 +37,8 @@ import { appRoutes } from './routes';
 		EventDetailsComponent,
 		CreateEventComponent,
 		Error404Component,
-		CreateSessionComponent
+		CreateSessionComponent,
+		SessionListComponent
     ],
     providers: [
 		EventService,
