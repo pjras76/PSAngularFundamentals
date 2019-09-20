984ffe51f76756dd9399a40168ac23eb1ae575bd
Rasmussen <Peter.Rasmussen@state.mn.us>
2019-09-20 09:48:04 -0500

    Module 13: clip 8 - Persisting Authentication across page refreshes

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,21 +1,44 @@
 import { Injectable } from '@angular/core'
 import { IUser } from './user.model'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable()
 export class AuthService {
     currentUser: IUser
 
-    loginUser(username: string, password: string) {
-        this.currentUser = {
-            id: 1,
-            userName: username,
-            firstName: 'John',
-            lastName: 'Papa'
-        }
+    constructor(private http: HttpClient) {
+
+    }
+
+    loginUser(userName: string, password: string) {
+        let loginInfo = { username: userName, password: password }
+        let options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' })}
+
+        return this.http.post('/api/login', loginInfo, options)
+            .pipe(tap(data => {
+                this.currentUser = <IUser>data['user']
+                
+            }))
+            .pipe(catchError(err => {
+                return of(false)
+            }))
+
     }
 
     isAuthenticated() {
         return !!this.currentUser;
+    }
+
+    checkAuthenticationStatus() {
+        this.http.get('/api/currentIdentity')
+                 .pipe(tap(data => {
+                    if(data instanceof Object) {
+                        this.currentUser = <IUser>data
+                    }
+                 }))
+                 .subscribe()
     }
 
     updateCurrentUser(formValues) {
